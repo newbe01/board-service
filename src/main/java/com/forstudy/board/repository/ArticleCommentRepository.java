b21de9b2f1347c41251281108d998505e74a8994
newbe01 <pkwaa3@gmail.com>
2023-03-19 20:51:08 +0900

    #6 - 테스트 코드에 newline 추가
    eof 추가

--- src/main/java/com/forstudy/board/repository/ArticleCommentRepository.java
+++ src/main/java/com/forstudy/board/repository/ArticleCommentRepository.java
@@ -4,4 +4,5 @@ import com.forstudy.board.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+
 }
--- src/test/java/com/forstudy/board/repository/JpaRepositoryTest.java
+++ src/test/java/com/forstudy/board/repository/JpaRepositoryTest.java
@@ -94,8 +94,5 @@ class JpaRepositoryTest {
         assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
         assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
 
-
     }
-
-
 }
