183fd6ebf2f1331e2aa016ab688b56774b05c06f
newbe01 <pkwaa3@gmail.com>
2023-03-19 20:52:10 +0900

    #6 - 테스트 코드에 newline 추가
    eof 추가

--- src/main/java/com/forstudy/board/repository/ArticleRepository.java
+++ src/main/java/com/forstudy/board/repository/ArticleRepository.java
@@ -4,4 +4,5 @@ import com.forstudy.board.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
 }
