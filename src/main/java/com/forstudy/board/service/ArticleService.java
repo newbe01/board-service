49be8a412095102ad313429941b6b568b2558c75
newbe01 <pkwaa3@gmail.com>
2023-03-29 05:05:46 +0900

    #21 - service
    파일 삭제

--- src/main/java/com/forstudy/board/service/ArticleService.java
+++ src/main/java/com/forstudy/board/service/ArticleService.java
@@ -2,7 +2,6 @@ package com.forstudy.board.service;
 
 import com.forstudy.board.domain.type.SearchType;
 import com.forstudy.board.dto.ArticleDto;
-import com.forstudy.board.dto.ArticleUpdateDto;
 import com.forstudy.board.dto.ArticleWithCommentsDto;
 import com.forstudy.board.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,9 +9,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional
