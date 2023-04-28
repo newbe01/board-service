7ef923465b15e1c3b459ec8beff5b6c389e29464
newbe01 <pkwaa3@gmail.com>
2023-04-28 21:50:08 +0900

    #70 - application.yaml : 환경변수 설정
    민감정보를 변수로 변경
    보안에 취약했던 부분을 외부에서 집어넣을수 있게 변경

--- src/main/java/com/forstudy/board/dto/security/BoardPrincipal.java
+++ src/main/java/com/forstudy/board/dto/security/BoardPrincipal.java
@@ -5,28 +5,29 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-//security 인증 및 권한 관리
 public record BoardPrincipal(
         String username,
         String password,
         Collection<? extends GrantedAuthority> authorities,
         String email,
         String nickname,
-<<<<<<< Updated upstream:src/main/java/com/forstudy/board/dto/BoardPrincipal.java
-        String memo
-) implements UserDetails {
-=======
         String memo,
         Map<String, Object> oAuth2Attributes
 ) implements UserDetails, OAuth2User {
->>>>>>> Stashed changes:src/main/java/com/forstudy/board/dto/security/BoardPrincipal.java
 
     public static BoardPrincipal of(String username, String password, String email, String nickname, String memo) {
+        return of(username, password, email, nickname, memo, Map.of());
+    }
+
+    public static BoardPrincipal of(String username, String password, String email, String nickname, String memo, Map<String, Object> oAuth2Attributes) {
+        // 지금은 인증만 하고 권한을 다루고 있지 않아서 임의로 세팅한다.
         Set<RoleType> roleTypes = Set.of(RoleType.USER);
 
         return new BoardPrincipal(
@@ -39,7 +40,8 @@ public record BoardPrincipal(
                 ,
                 email,
                 nickname,
-                memo
+                memo,
+                oAuth2Attributes
         );
     }
 
@@ -73,6 +75,8 @@ public record BoardPrincipal(
     @Override public boolean isCredentialsNonExpired() { return true; }
     @Override public boolean isEnabled() { return true; }
 
+    @Override public Map<String, Object> getAttributes() { return oAuth2Attributes; }
+    @Override public String getName() { return username; }
 
     public enum RoleType {
         USER("ROLE_USER");
